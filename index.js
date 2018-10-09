7f91a80cd84c5cc71b34fe7b35d8bf44f2dab893
Edd <edd@ukldn01842mac.local>
2018-10-09 11:07:42 +0100

    add support for export default compiled via babel

--- index.js
+++ index.js
@@ -5,24 +5,28 @@ const {
     NODE_ENV
 } = process.env
 
-const isProd = NODE_ENV === "production"
-
+const isProd = NODE_ENV === 'production'
 
 module.exports.routesPlugin = (fastify)=>{
-      // -----------Register routes ----------- //
-      const routesDir = path.join(process.cwd(), isProd ? './.build.routes' : './routes')
-      //todo: add check routes dir exists
+    // -----------Register routes ----------- //
+    const routesDir = path.join(process.cwd(), isProd ? './.build.routes' : './routes')
+    // todo: add check routes dir exists
 
-      readDirR(routesDir)
-          .filter(
-              file =>
-                  file.indexOf('.') !== 0 &&
-              //file !== basename &&
+    readDirR(routesDir)
+        .filter(
+            file =>
+                file.indexOf('.') !== 0 &&
+              // file !== basename &&
               file.slice(-3) === '.js'
-          )
-          .forEach(file => fastify.register(require(file), {
-              prefix: ROUTES_PREFIX || ''
-          }))
-      // -----------Register routes ----------- //
+        )
+        .forEach(file => {
+            const route = require(file)
+            // todo: add check type of route and better support for babel compiled
+            fastify.register(typeof route.default !== 'undefined' ? route.default : route, {
+                prefix: ROUTES_PREFIX || ''
+            })
+        
+        })
+    // -----------Register routes ----------- //
 }
 module.exports.fastifySequelizeAndRoutesPlugin = require('./sequelize').fastifySequelizeAndRoutesPlugin
