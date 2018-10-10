7e2f276f611993e47a14afa09beadfab4b1c8315
etidbury <tidbury@live.com>
2018-10-10 15:59:11 +0100

    allow require('sequelize/config') instead of dodgy combination of require and object specification

---
+++ sequelize/config/index.js
@@ -0,0 +1,53 @@
+// const isProd = process.env.NODE_ENV === 'production'
+
+// ensure all required env variables are set for MySQL connection
+['MYSQL_HOST', 'MYSQL_DB_NAME', 'MYSQL_USER', 'MYSQL_PASSWORD'].forEach(
+    envVarName => {
+        if (typeof process.env[envVarName] === 'undefined')
+            throw new ReferenceError(
+                `Required environment variable not set: ${envVarName}`
+            )
+    }
+)
+
+const {
+    MYSQL_HOST
+    , MYSQL_DB_NAME
+    , MYSQL_USER
+    , MYSQL_PASSWORD
+    , DISABLE_LOGS
+    , SEQUELIZE_LOGGING
+} = process.env
+
+module.exports = {
+    dialect: 'mysql',
+    // "use_env_variable": "MYSQL_CONNECTION_URL",
+    host: MYSQL_HOST,
+    database: MYSQL_DB_NAME,
+    username: MYSQL_USER,
+    password: MYSQL_PASSWORD,
+    autoConnect: true, // auto authentication and test connection on first run
+    // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+    operatorsAliases: false,
+    logging: !DISABLE_LOGS && function(msg){
+        console.debug('> Sequelize:',msg)
+    },
+    
+    freezeTableName: true,
+    // disable inserting undefined values as NULL
+    // - default: false
+    omitNull: true,
+    
+    define: {
+        freezeTableName: false,
+        charset: 'utf8',
+        dialectOptions: {
+            collate: 'utf8_general_ci'
+        },
+        timestamps: true
+    },
+    // for emojis:
+    // charset: 'utf8mb4',
+    // collate: 'utf8mb4_unicode_ci'
+    // logging: console.debug
+}
