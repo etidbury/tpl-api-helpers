80d7a8fd27b87c17fb416878be443258007bf725
etidbury <tidbury@live.com>
2018-10-16 11:32:12 +0100

    add sequelize JSON support in customEntityProps method for mysql <=5.6

--- util/sequelize/index.js
+++ util/sequelize/index.js
@@ -5,53 +5,66 @@ module.exports.customEntityProps = (entityProps)=>{
     Object.keys(entityProps).forEach((fieldName)=>{
 
         // transform arrays into text
-        if (entityProps[fieldName] === DataTypes.ARRAY){
-            entityProps[fieldName] = {
-                type: new DataTypes.STRING,
-                set(val) {
-        
-                    if (typeof val === 'object'){
-                        val = val.join(',')
-                    }
-        
-                    this.setDataValue(fieldName, val)
-                    
-                },
-                get(){
-                    return this.getDataValue(fieldName).split(',')
+        if (entityProps[fieldName] === DataTypes.ARRAY || (entityProps[fieldName].type && entityProps[fieldName].type === DataTypes.ARRAY)) {
+
+            if (typeof entityProps[fieldName] !== 'object'){
+                entityProps[fieldName] = {}
+            }
+
+            entityProps[fieldName].type = new DataTypes.TEXT()
+
+            entityProps[fieldName].set = function(val) {
+                
+                if (typeof val === 'object'){
+                    val = val.join(',')
+                }
+    
+                this.setDataValue(fieldName, val)
+                
+            }
+            entityProps[fieldName].get = function() {
+                return this.getDataValue(fieldName).split(',')
+            }
+
+        }
+
+        // note: JSON for MySQL is supported as of Sequelize 4.18 therefore below not needed unless
+        // also note, Support only from MySQL versions => 5.7
+
+        // transform json into text
+        if (
+            entityProps[fieldName] === DataTypes.JSON 
+            || (entityProps[fieldName].type && entityProps[fieldName].type === DataTypes.JSON)
+            || (entityProps[fieldName].type && entityProps[fieldName].type.key && entityProps[fieldName].type.key === 'JSON')
+        ) {
+            
+            if (typeof entityProps[fieldName] !== 'object'){
+                entityProps[fieldName] = {}
+            }
+
+            entityProps[fieldName].type = new DataTypes.TEXT()
+
+            entityProps[fieldName].set = function(val) {
+              
+                if (typeof val === 'object'){
+                    val = JSON.stringify(val)
+                }else{
+                    throw new TypeError('customEntityProps(): Invalid value specified for field name: ',fieldName,' must be JSON format')
+                }
+                
+                this.setDataValue(fieldName, val)
+            }
+
+            entityProps[fieldName].get = function() {
+                try{
+                    return JSON.parse(this.getDataValue(fieldName))
+                } catch(err){
+                    console.error(`customEntityProps(): Failed to parse JSON field named '${fieldName}'`)
+                    throw err
                 }
             }
         }
 
-        // note: JSON for MySQL is supported as of Sequelize 4.18 therefore below not needed unless using other engine
-
-        // // transform object into text
-        // if (entityProps[fieldName] === DataTypes.JSON){
-        //     entityProps[fieldName] = {
-        //         type: new DataTypes.TEXT(),
-        //         set(val) {
-        
-        //             console.log('json...',fieldName,typeof val,val)
-
-        //             if (typeof val === 'object'){
-        //                 val = JSON.stringify(val)
-        //             }else{
-        //                 val = JSON.stringify(JSON.parse(val))
-        //             }
-                    
-        //             this.setDataValue(fieldName, val)
-                    
-        //         },
-        //         get(){
-        //             try{
-        //                 return JSON.parse(this.getDataValue(fieldName))
-        //             } catch(err){
-        //                 console.error(`customEntityProps: Failed to parse JSON field named '${fieldName}'`)
-        //                 throw err
-        //             }
-        //         }
-        //     }
-        // }
     })
 
     return entityProps
